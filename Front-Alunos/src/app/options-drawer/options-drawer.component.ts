6ad49b489003171aa8db997f2832e1a057cb6ef8
Kinderman Barros <kindbarros@gmail.com>
2024-10-08 14:35:26 -0400

    Added Bootstrap to provice more consistent styling

---
+++ Front-Alunos/src/app/options-drawer/options-drawer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-options-drawer',
+  standalone: true,
+  imports: [],
+
+  templateUrl: './options-drawer.component.html',
+  styleUrl: './options-drawer.component.css',
+})
+export class OptionsDrawerComponent {}
